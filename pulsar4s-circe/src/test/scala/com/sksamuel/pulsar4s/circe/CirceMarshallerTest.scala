04db3f8182031854291646d14108e525cbd67b20
sksamuel <sam@sksamuel.com>
2018-01-10 23:47:20 +0000

    Added more tests for circe marshallers

---
+++ pulsar4s-circe/src/test/scala/com/sksamuel/pulsar4s/circe/CirceMarshallerTest.scala
@@ -0,0 +1,26 @@
+package com.sksamuel.pulsar4s.circe
+
+import com.sksamuel.pulsar4s.Message
+import org.scalatest.{Matchers, WordSpec}
+
+class CirceMarshallerTest extends WordSpec with Matchers {
+
+  import com.sksamuel.pulsar4s.{MessageReader, MessageWriter}
+  import io.circe.generic.auto._
+
+  "A derived MessageWriter instance" should {
+    "create a message from a class" in {
+      val msg = implicitly[MessageWriter[Cafe]].write(Cafe("le table", Place(1, "Paris"))).right.get
+      msg.key shouldBe None
+      msg.messageId shouldBe None
+      new String(msg.data) shouldBe """{"name":"le table","place":{"id":1,"name":"Paris"}}"""
+    }
+  }
+
+  "A derived MessageReader instance" should {
+    "read a class from a message" in {
+      implicitly[MessageReader[Cafe]].read(Message("""{"name":"le table","place":{"id":1,"name":"Paris"}}""")) shouldBe
+        Right(Cafe("le table", Place(1, "Paris")))
+    }
+  }
+}
