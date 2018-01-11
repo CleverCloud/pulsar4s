7e5aaf3ece377856968759d1052e3ff0135af1e1
sksamuel <sam@sksamuel.com>
2018-01-11 23:26:25 +0000

    Implemented jackson marshaller tests

--- pulsar4s-jackson/src/test/scala/com/sksamuel/pulsar4s/jackson/JacksonTest.scala
+++ pulsar4s-jackson/src/test/scala/com/sksamuel/pulsar4s/jackson/JacksonTest.scala
@@ -1,14 +1,29 @@
 package com.sksamuel.pulsar4s.jackson
 
-import org.scalatest.FunSuite
+import com.sksamuel.pulsar4s.Message
+import org.scalatest.{Matchers, WordSpec}
 
-class JacksonTest extends FunSuite {
+case class Place(id: Int, name: String)
+case class Cafe(name: String, place: Place)
 
-  test("jackson should marshall T to message") {
+class JacksonTest extends WordSpec with Matchers {
 
+  import Jackson._
+  import com.sksamuel.pulsar4s.{MessageReader, MessageWriter}
+
+  "A derived MessageWriter instance" should {
+    "create a message from a class" in {
+      val msg = implicitly[MessageWriter[Cafe]].write(Cafe("le table", Place(1, "Paris"))).get
+      msg.key shouldBe None
+      msg.messageId shouldBe None
+      new String(msg.data) shouldBe """{"name":"le table","place":{"id":1,"name":"Paris"}}"""
+    }
   }
 
-  test("jackson should unmarshall message to T") {
-
+  "A derived MessageReader instance" should {
+    "read a class from a message" in {
+      implicitly[MessageReader[Cafe]].read(Message("""{"name":"le table","place":{"id":1,"name":"Paris"}}""")).get shouldBe
+        Cafe("le table", Place(1, "Paris"))
+    }
   }
 }
