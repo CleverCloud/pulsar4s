b43db9143c9ad50a36a955db614489ed3b340156
sksamuel <sam@sksamuel.com>
2018-09-17 04:51:37 +0100

    Added missing methods to Reader

--- pulsar4s-core/src/main/scala/com/sksamuel/pulsar4s/Reader.scala
+++ pulsar4s-core/src/main/scala/com/sksamuel/pulsar4s/Reader.scala
@@ -1,25 +1,40 @@
 package com.sksamuel.pulsar4s
 
+import java.io.Closeable
 import java.util.concurrent.TimeUnit
 
 import scala.concurrent.duration.Duration
 import scala.language.higherKinds
 
-class Reader[T](reader: org.apache.pulsar.client.api.Reader[T],
-                val topic: Topic) {
+trait Reader[T] extends Closeable {
+  def hasMessageAvailable: Boolean
+  def topic: Topic
+  def next: ConsumerMessage[T]
+  def next(duration: Duration): Option[ConsumerMessage[T]]
+  def nextAsync[F[_] : AsyncHandler]: F[ConsumerMessage[T]]
+  def closeAsync[F[_] : AsyncHandler]: F[Unit]
+  def hasReachedEndOfTopic: Boolean
+}
 
-  def next: ConsumerMessage[T] = ConsumerMessage.fromJava(reader.readNext)
+class DefaultReader[T](reader: org.apache.pulsar.client.api.Reader[T],
+                       override val topic: Topic) extends Reader[T] {
 
-  def next(duration: Duration): Option[ConsumerMessage[T]] =
+  override def hasMessageAvailable: Boolean = reader.hasMessageAvailable
+
+  override def next: ConsumerMessage[T] = ConsumerMessage.fromJava(reader.readNext)
+
+  override def next(duration: Duration): Option[ConsumerMessage[T]] =
     Option(reader.readNext(duration.toSeconds.toInt, TimeUnit.SECONDS)).map(ConsumerMessage.fromJava)
 
-  def nextAsync[F[_] : AsyncHandler]: F[ConsumerMessage[T]] = implicitly[AsyncHandler[F]].nextAsync(reader)
+  override def nextAsync[F[_] : AsyncHandler]: F[ConsumerMessage[T]] = implicitly[AsyncHandler[F]].nextAsync(reader)
 
-  def closeAsync[F[_] : AsyncHandler]: F[Unit] = implicitly[AsyncHandler[F]].close(reader)
+  override def close(): Unit = reader.close()
+  override def closeAsync[F[_] : AsyncHandler]: F[Unit] = implicitly[AsyncHandler[F]].close(reader)
 
   /**
     * Returns true if the topic was terminated and this reader
     * has reached the end of the topic.
     */
-  def hasReachedEndOfTopic: Boolean = reader.hasReachedEndOfTopic
+  override def hasReachedEndOfTopic: Boolean = reader.hasReachedEndOfTopic
+
 }
