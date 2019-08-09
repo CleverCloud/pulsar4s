2fb4cf36bfa76dafab9238779c9327c930b9a8b7
Scala Steward <me@scala-steward.org>
2019-08-09 22:26:59 +0200

    Update akka-stream to 2.5.24

--- project/Build.scala
+++ project/Build.scala
@@ -11,7 +11,7 @@ object Build extends AutoPlugin {
 
   object autoImport {
     val org                       = "com.sksamuel.pulsar4s"
-    val AkkaStreamVersion         = "2.5.23"
+    val AkkaStreamVersion         = "2.5.24"
     val CatsEffectVersion         = "1.4.0"
     val CirceVersion              = "0.11.1"
     val CommonsIoVersion          = "2.4"
