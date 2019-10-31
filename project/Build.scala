988399231741826bbfc1c22a41271b17fc2b921f
Scala Steward <me@scala-steward.org>
2019-10-31 07:11:18 +0100

    Update zio-interop-cats to 2.0.0.0-RC7

--- project/Build.scala
+++ project/Build.scala
@@ -29,7 +29,7 @@ object Build extends AutoPlugin {
     val Slf4jVersion              = "1.7.28"
     val SprayJsonVersion          = "1.3.5"
     val Java8CompatVersion        = "0.9.0"
-    val ZioInteropVersion         = "2.0.0.0-RC6"
+    val ZioInteropVersion         = "2.0.0.0-RC7"
   }
 
   import autoImport._
