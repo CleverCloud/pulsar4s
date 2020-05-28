375709f0887bdda7ad41949d65f83c18a01bd093
nicolo.paganin <nicolo.paganin@oncode.it>
2020-05-28 12:20:15 +0200

    implemetation of startMessageFromRollbackDuration and startMessageIdInclusive

---
+++ pulsar4s-core/src/main/scala/com/sksamuel/pulsar4s/ReaderConfig.scala
@@ -0,0 +1,21 @@
+package com.sksamuel.pulsar4s
+
+import org.apache.pulsar.client.api._
+
+import scala.concurrent.duration.FiniteDuration
+import scala.util.matching.Regex
+import java.util.concurrent.TimeUnit
+
+sealed trait StartMessage
+final case class Message(messageId: MessageId) extends StartMessage
+final case class RollBack(rollbackDuration: Long, timeunit: TimeUnit) extends StartMessage
+
+case class ReaderConfig(topic: Topic,
+                        @deprecated("use startMessage instead", "2.5.3")
+                        seek: MessageId = MessageId.earliest,
+                        startMessage: StartMessage,
+                        startMessageIdInclusive: Boolean = true,
+                        receiverQueueSize: Option[Int] = None,
+                        reader: Option[String] = None,
+                        readCompacted: Option[Boolean] = None,
+                        additionalProperties: Map[String, AnyRef] = Map.empty)
